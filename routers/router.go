7a091e6e1eb8a0b4ad266488f6fea70cee50def2
mingya.zhang <mingya.zhang@horizon.ai>
2019-04-18 10:44:50 +0800

    jwt confirm ID

---
+++ routers/router.go
@@ -0,0 +1,38 @@
+package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	"myginExample/pkg/setting"
+	"myginExample/routers/api/v1"
+)
+
+func InitRouter() *gin.Engine {
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+	gin.SetMode(setting.RunMode)
+
+	apiv1 := r.Group("/api/v1")
+	{
+		apiv1.GET("/tags", v1.GetTags)
+
+		apiv1.POST("/tags", v1.AddTags)
+
+		apiv1.PUT("/tags/:id", v1.EditTag)
+
+		apiv1.DELETE("/tags/:id", v1.DeleteTag)
+
+		apiv1.GET("/articles", v1.GetArticles)
+
+		apiv1.GET("/articles/:id", v1.GetArticle)
+
+		apiv1.POST("/articles", v1.AddArticle)
+
+		apiv1.PUT("/articles/:id", v1.EditArticle)
+
+		apiv1.DELETE("/articles/:id", v1.DeleteArticle)
+
+	}
+
+	return r
+}
